1b0ee8d7710bc59aba8b273ef000ff096fe61e78
Ian Crane <ian.crane12@gmail.com>
2023-02-09 10:23:08 -0600

    completed explore exercises

--- acquire.py
+++ acquire.py
@@ -130,3 +130,31 @@ def get_auto_mpg():
     df['horsepower'] = df['horsepower'].astype('float')
     
     return df
+
+
+def acquire_store():
+    
+    filename = 'store.csv'
+    
+    if os.path.exists(filename):
+        
+        return pd.read_csv(filename)
+    
+    else:
+        
+        query = '''
+                SELECT sale_date, sale_amount,
+                item_brand, item_name, item_price,
+                store_address, store_zipcode
+                FROM sales
+                LEFT JOIN items USING(item_id)
+                LEFT JOIN stores USING(store_id)
+                '''
+        
+        url = get_connection(db='tsa_item_demand')
+        
+        df = pd.read_sql(query, url)
+        
+        df.to_csv(filename, index=False)
+        
+        return df
